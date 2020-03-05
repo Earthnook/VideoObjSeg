694b070aa5049ce1f5f8f7ad9777fcef35946949
ZiwenZhuang <zhuangzw@shanghaitech.edu.cn>
2020-03-05 13:01:57 +0800

    change mask image logging method

--- vos/runner/video_mask.py
+++ vos/runner/video_mask.py
@@ -15,49 +15,57 @@ class VideoMaskRunner(RunnerBase):
         """ For the memory efficiency, this will only randomly choose `n_select_frames` of frames
         to store.
         """
-        if not hasattr(self, "_extra_infos"):
+        if not hasattr(self, "_extra_infos") or self._extra_infos is None:
             # a hacky way of initialization
-            self._extra_infos = {k: list() for k in ["images", "preds", "results"]}
+            self._extra_infos = {k: list() for k in ["images", "masks", "preds"]}
 
         videos = extra_info["videos"]
+        masks = extra_info["masks"]
         preds = extra_info["preds"]
 
         _, T, C, H, W = videos.shape
+        _, _, N, _, _ = masks.shape
         _, _, n, _, _ = preds.shape
 
         # select frames
         t_i = np.random.choice(T, n_select_frames)
-        videos = videos[:, t_i]
+        images = videos[:, t_i]
+        masks = masks[:, t_i]
         preds = preds[:, t_i]
 
         images = videos.reshape((-1, C, H, W))
+        masks = masks.reshape((-1, N, H, W))
         preds = preds.reshape((-1, n, H, W))
 
-        masked_images = overlay_images(images, preds, alpha= 0.2)
         self._extra_infos["images"].extend([image for image in images])
+        self._extra_infos["masks"].extend([image for image in masks])
         self._extra_infos["preds"].extend([image for image in preds])
-        self._extra_infos["results"].extend([image for image in masked_images])
 
     def _log_extra_info(self, itr_i):
         # transpose from (b, C, H, W) to (b, H, W, C)
         images = np.stack(self._extra_infos["images"], axis= 0).transpose(0,2,3,1)
-        results = np.stack(self._extra_infos["results"], axis= 0).transpose(0,2,3,1)
-        # due to multi-channel, preds are a bit different
+        # due to multi-channel, masks and preds are a bit different
+        masks = [np.expand_dims(np.hstack(x[:]), axis= 2) \
+            for x in self._extra_infos["masks"]] # a list of (H, n*W, 1)
         preds = [np.expand_dims(np.hstack(x[:]), axis= 2) \
             for x in self._extra_infos["preds"]] # a list of (H, n*W, 1)
 
         # write to summary file
         tf_image_summary("input images", data=images, step= itr_i)
         for pred in preds:
+            tf_image_summary("ground truths",
+                data= np.expand_dims(masks, axis= 0) * 255,
+                step= itr_i
+            )
+        for pred in preds:
             tf_image_summary("predictions",
                 data= np.expand_dims(pred, axis= 0) * 255,
                 step= itr_i
             )
-        tf_image_summary("masked images", data= results, step= itr_i)
 
         # reset
         del self._extra_infos
-        self._extra_infos = {k: list() for k in ["images", "preds", "results"]}
+        self._extra_infos = None
         
     def log_data_info(self, itr_i, data):
         """ In case of data pre-processing bugs, log images into tensorflow
