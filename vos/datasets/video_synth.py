ba03fba8b07916a61fb7cc1d83b2d634f93f1210
ZiwenZhuang <zhuangzw@shanghaitech.edu.cn>
2020-03-18 00:58:33 +0800

    finish integrating more data-aug method into video_synth

--- vos/datasets/video_synth.py
+++ vos/datasets/video_synth.py
@@ -1,4 +1,5 @@
 import torch
+from torch.nn import functional as F
 from torch.utils.data import Dataset
 from torchvision import transforms
 from torchvision.transforms import functional as visionF
@@ -6,6 +7,7 @@ import numpy as np
 
 from vos.utils.quick_args import save__init__args
 from vos.utils.image_shaper import random_crop_CHW
+from vos.utils.tps_transform import image_tps_transform
 
 class VideoSynthDataset(Dataset):
     """ A wrapper that synthesize image dataset to a video-like dataset by randomly jitter
@@ -21,12 +23,17 @@ class VideoSynthDataset(Dataset):
                 scale_max= 2., # NOTE: this is the exponent of e
                 shear_max= 50.
             ), # a dict of kwargs providing for torchvision.transforms.functional.affine
+            TPS_kwargs= dict(
+                scale= 0.1,
+                n_points= 5, # no less than 3
+            ),
+            dilate_scale= 5, # the number of pixels to dilate the masks
         ):
         save__init__args(locals())
         self.to_pil_image = transforms.ToPILImage()
         self.to_tensor = transforms.ToTensor()
 
-    def random_transforms(self, image, mask):
+    def random_affine_transform(self, image, mask):
         """ randomly generate a transform arguments, and apply it to both image and mask.
         Considering torchvision transform can only transform a single image, in requires
         some meanuvers to do with mask.
@@ -81,6 +88,42 @@ class VideoSynthDataset(Dataset):
 
         return image, mask
 
+    def random_tps_transform(self, image, mask):
+        """ Apply a random TPS mapping onto both image and mask (they have the same TPS transformation)
+        """
+        image = image.numpy()
+        mask = mask.numpy()
+
+        _, ys, xs = np.nonzero(mask[1:] == 1)
+        idxs = np.random.choice(len(xs), self.TPS_kwargs["n_points"])
+        interest_cps = np.vstack((xs[idxs], ys[idxs])).T
+
+        jitter_scale = np.sqrt(xs.shape[0]) / (mask.shape[0]-1) * self.TPS_kwargs["scale"]
+        target_cps = interest_cps + \
+            np.random.uniform(-jitter_scale, jitter_scale, size= interest_cps.shape)
+
+        tps_image = image_tps_transform(image, interest_cps, target_cps)
+        tps_mask = image_tps_transform(mask, interest_cps, target_cps)
+
+        return torch.from_numpy(tps_image), torch.from_numpy(tps_mask)
+
+    def dilate_mask(self, masks):
+        """ NOTE: each mask must have background layer at 0-th channel
+        """
+        masks = masks.to(dtype= torch.float32)
+        
+        _, C, _, _ = masks.shape
+        k_size = 2 * self.dilate_scale + 1
+        dilate_kernel = torch.ones((C-1, 1, k_size, k_size))
+        with torch.no_grad():
+            masks[:,1:] = F.conv2d(masks[:,1:], dilate_kernel,
+                padding= self.dilate_scale,
+                groups= C-1,
+            )
+
+        masks = masks.to(dtype= torch.uint8)
+        return masks
+
     def synth_videos(self, images, masks):
         """ Synthesize video clips by torch images. Return a torch.Tensor as a batch of
         video clips
@@ -90,11 +133,12 @@ class VideoSynthDataset(Dataset):
             for image, mask in zip(images, masks):
                 video, m_video = [image], [mask]
                 for frame_i in range(self.n_frames-1):
-                    frame, m_frame = self.random_transforms(image, mask)
+                    frame, m_frame = self.random_affine_transform(image, mask)
+                    frame, m_frame = self.random_tps_transform(frame, m_frame)
                     video.append(frame)
                     m_video.append(m_frame)
                 videos.append(torch.stack(video))
-                m_videos.append(torch.stack(m_video))
+                m_videos.append(self.dilate_mask(torch.stack(m_video)))
         videos = torch.stack(videos)
         m_videos = torch.stack(m_videos)
         # the returned videos should be batch-wise
