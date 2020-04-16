b46d5a5e3e0a02c08ed3e82460b1fba722c289ef
ZiwenZhuang <zhuangzw@shanghaitech.edu.cn>
2020-04-16 15:31:53 +0800

    fix calc_performance when n_objects are different among batch

--- vos/algo/videoobjseg.py
+++ vos/algo/videoobjseg.py
@@ -39,11 +39,15 @@ class VideoObjSegAlgo(AlgoBase):
         """
         raise NotImplementedError
 
-    def calc_performance(self, pred, gtruth, smooth= 1):
-        """ Given the statistics (in same shape (N, ...)) and caculate average value in this batch
+    def calc_performance(self, pred, gtruth, n_objects, smooth= 1):
+        """ Given the statistics and caculate average value in this batch
             NOTE: the average is only taken on 0-th dimension
             Calculation refering to book: 
             Pattern Recognition and Computer Vision: Second Chinese Conference, PRCV page 423
+        @ Args:
+            pred: ndarray with shape (B, T, N, H, W) with or without background channel
+            grtuth: ndarray with the same shape as pred
+            n_objects: ndarray with shape (B,) due to different n_objects in each data among batch
         """
         gtruth = gtruth.astype(np.bool)
         pred = pred.astype(np.bool)
@@ -51,21 +55,40 @@ class VideoObjSegAlgo(AlgoBase):
         union = np.sum(gtruth | pred, axis= (-2, -1)) + smooth
 
         # calculate region similarity (a.k.a Intersection over Unit)
-        IoU = intersect / union
+        IoU = intersect / union # (B, T, N)
+        # calculate average IoU for each data in this batch
+        IoU_mean = [] # a list of (T*n,)
+        for b_i, iou in enumerate(IoU):
+            if not self.include_bg_loss:
+                iou_ = iou[:, :n_objects[b_i]]
+            else:
+                iou_ = iou[:, :(n_objects[b_i]+1)]
+            IoU_mean.append(iou_.flatten())
+        IoU_mean = np.nanmean(np.hstack(IoU_mean))
+
         # This modification referring to https://github.com/fperazzi/davis/blob/master/python/lib/davis/measures/jaccard.py#L29
         # And https://github.com/suhwan-cho/davis-evaluation/blob/master/davis2017/metrics.py#L37
-        IoU[np.isclose(union, 0)] = 1
+        # NOTE: no need, smooth=1 make the same effect
+        # IoU[np.isclose(union, 0)] = 1
 
         # calculate contour accuracy
         # NOTE: This calculation is not accurate due to experiment speed, 
         # please check official evaluation server
         accuracy_rate = intersect / (np.sum(pred, axis= (-2, -1)) + smooth)
         recall_rate = intersect / (np.sum(gtruth, axis= (-2, -1)) + smooth)
-        contour_acc = 2 * (accuracy_rate * recall_rate) / (accuracy_rate + recall_rate)
+        contour_acc = 2 * (accuracy_rate * recall_rate) / (accuracy_rate + recall_rate) # (B, T, N)
+        acc_mean = [] # a list of (T*n,)
+        for b_i, acc in enumerate(contour_acc):
+            if not self.include_bg_loss:
+                acc_ = acc[:, :n_objects[b_i]]
+            else:
+                acc_ = acc[:, :(n_objects[b_i]+1)]
+            acc_mean.append(acc_.flatten())
+        acc_mean = np.nanmean(np.hstack(acc_mean))
 
         return dict(
-            IoU= np.nanmean(IoU),
-            contour_acc= np.nanmean(contour_acc),
+            IoU= IoU_mean,
+            contour_acc= acc_mean,
             IoU_each_frame= IoU, # add this term for debugging, will not effect logging mechanism
             contour_acc_frame= contour_acc,
         )
@@ -93,9 +116,9 @@ class VideoObjSegAlgo(AlgoBase):
 
         loss_idx = 0 if self.include_bg_loss else 1
         _, _, n, H, W = gtruths.shape
-        p = preds[:,1:,loss_idx:].reshape(-1, n-1, H, W)
-        g = gtruths[:,1:,loss_idx:].reshape(-1, n-1, H, W)
-        performance_status = self.calc_performance(p, g)
+        # p = preds[:,1:,loss_idx:].reshape(-1, n-1, H, W)
+        # g = gtruths[:,1:,loss_idx:].reshape(-1, n-1, H, W)
+        performance_status = self.calc_performance(preds, gtruths, n_objects= data["n_objects"])
     
         return TrainInfo(
                 loss= loss.detach().cpu().numpy(), 
@@ -133,9 +156,9 @@ class VideoObjSegAlgo(AlgoBase):
 
         loss_idx = 0 if self.include_bg_loss else 1
         _, _, n, H, W = gtruths.shape
-        p = preds[:,1:,loss_idx:].reshape(-1, n-1, H, W)
-        g = gtruths[:,1:,loss_idx:].reshape(-1, n-1, H, W)
-        performance_status = self.calc_performance(p, g)
+        # p = preds[:,1:,loss_idx:].reshape(-1, n-1, H, W)
+        # g = gtruths[:,1:,loss_idx:].reshape(-1, n-1, H, W)
+        performance_status = self.calc_performance(preds, gtruths, n_objects= data["n_objects"])
 
         return EvalInfo(
                 loss= loss.cpu().numpy(),
